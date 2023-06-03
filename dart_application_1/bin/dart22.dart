2b444e91f4fab56b6e096e081cf25151c0d53c0c
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-03 22:16:18 +0900

    Async Programming(await)

--- dart_application_1/bin/dart22.dart
+++ dart_application_1/bin/dart22.dart
@@ -1,6 +1,6 @@
-void main() {
-  addNumbers(1, 1);
-  addNumbers(2, 2);
+void main() async {
+  await addNumbers(1, 1);
+  await addNumbers(2, 2);
 
   // Future
   Future<String> name = Future.value('zimmyrabbit');
@@ -10,19 +10,19 @@ void main() {
   // delayed
   // 1번 파라미터 - 지연할 기간 (얼마나 지연할건지) Duration
   // 2번 파라미터 - 지연 시간이 지난 후 실행할 함수
-  print('함수 시작');
-  Future.delayed(Duration(seconds: 2),(){
-    print('Delay 끝');
-  });
+  //print('함수 시작');
+  //Future.delayed(Duration(seconds: 2),(){
+  //  print('Delay 끝');
+  //});
 }
 
 // 1 + 1 = 2
 // 2 + 2 = 4
-void addNumbers(int number1, int number2) {
+Future<void> addNumbers(int number1, int number2) async {
   print('계산 시작: $number1 + $number2');
 
   //서버 시뮬레이션
-  Future.delayed(Duration(seconds: 2),() {
+  await Future.delayed(Duration(seconds: 2),() {
     print('계산 완료 : $number1 + $number2 = ${number1 + number2}');
   });
   print('함수 완료');
